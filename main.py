5413f364b1665d6a8b965c829d8cec30af2df265
Sajid <sajid@duck.com>
2024-05-02 01:53:10 +0530

    Added images in frames - idle and more animation code

--- main.py
+++ main.py
@@ -8,23 +8,57 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Target #Error')
 
+#Game FRAME RATE
+clock = pygame.time.Clock()
+FPS = 60
+
 #player action
 moving_left = False
 moving_right = False
 
+#COLORS
+BG = (144, 201, 120)
+
+def draw_bg():
+    screen.fill(BG)
+
 class Soldier(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale, ):
+    def __init__(self, char_type, x, y, scale, speed, ):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('./assets/player_character/Gunner_Black_Idle.png').convert_alpha()
+        self.char_type = char_type
+        self.speed = speed        
+        self.direction = 1
+        self.flip = False
+        img = pygame.image.load(f'./assets/{self.char_type}/idle/0.png')
         self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
     
+    def move(self, moving_left, moving_right):
+        #reset movement var
+        dx = 0
+        dy = 0
+        
+        #move left and right
+        if moving_left:
+            dx = -self.speed    
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+            
+        #update position
+        self.rect.x += dx
+        self.rect.y += dy
+        
+    
     def draw(self):
-        screen.blit(self.image, player.rect)
-    
-    
-player = Soldier(200, 200, 3)
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+player = Soldier('player_character', 200, 200, 3, 5)
+enemy = Soldier('player_character', 400, 200, 3, 5)
 
 
 x = 200
@@ -35,7 +69,14 @@ run = True
 
 while run:
     
+    clock.tick(FPS)
+    
+    draw_bg()
+    
     player.draw()
+    enemy.draw()
+    
+    player.move(moving_left, moving_right)
     
     for event in pygame.event.get():
         #button presses
